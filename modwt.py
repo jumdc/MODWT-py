8dd8bba88f26ae74d3c090e5c9ac3020042ec571
jumdc <julie.mordacq@live.fr>
2022-09-20 19:04:30 +0200

    add mra
    TODO : issues with MRA

--- modwt.py
+++ modwt.py
@@ -1,12 +1,13 @@
 import stat
 import pywt._wavelet_packets
+import json
 import pywt.data
 import numpy as np
 import bioread
 from math import log2
 import matplotlib.pyplot as plt
 
-class MODWT: 
+class WaveletsTransforms: 
     """Performs MODWT 
     (maximum overlap discrete wavelet transform)
 
@@ -31,47 +32,44 @@ class MODWT:
             J -= 1
         print(J)
         return J
+
+    def get_wavelet_filters(self, path):
+        with open(path, 'r') as file : 
+            data = json.load(file)
+        self.wavelets = data
     
-    def modwt(self):
-        h = [
-            -0.0105974018, 0.0328830117, 0.0308413818, 
-            -0.1870348117, -0.0279837694, 0.6308807679, 
-            0.7148465706, 0.2303778133
-            ]
-        g = [
-            -0.2303778133, 0.7148465706, -0.6308807679,
-            -0.0279837694, 0.1870348117, 0.0308413818, 
-            -0.0328830117, -0.0105974018
-        ]
+    def modwt(self, filter):
+        h = self.wavelets[filter]['h']
+        g = self.wavelets[filter]['g']
+
+        print(h, g)
 
         N = len(self.data)
-        V = []  # list for scaling coefficients. 
-        V.append(self.data)
+        V = [self.data]  # list for scaling coefficients. 
         W = []
         L = len(h)
 
         for scale in range(1,self.J + 1) : 
-            print("scale", scale)
             W_scale = []
             V_scale = []
             # N = len(V[-1]) # the previous coef length. 
             for t in range(N):
                 k = 0
                 k = t
-                W_scale_t = h[0]*V[-1][k]
-                V_scale_t = g[0]*V[-1][k]
-                for n in range(1, L ): 
-                    k = k - 2**(scale-1)
+                W_scale_t = h[0] * V[-1][k]
+                V_scale_t = g[0] * V[-1][k]
+                for n in range(1, L): 
+                    k = k - 2 ** (scale-1)
                     if k < 0 : # circular shift
                         k = k + N 
-                    W_scale_t += h[n]*V[-1][k] # n 
-                    V_scale_t += h[n]*V[-1][k] # n
+                    W_scale_t += h[n] * V[-1][k] # n 
+                    V_scale_t += h[n] * V[-1][k] # n
                 W_scale.append(W_scale_t)
                 V_scale.append(V_scale_t)
             V.append(V_scale)
             W.append(W_scale)
         return V[-1], W
-
+ 
     @staticmethod
     def shift_factor_H(j, L): 
         """
@@ -108,9 +106,6 @@ class MODWT:
     
     @staticmethod
     def circular_shift(factor, data):
-        print(data[:10], data[-10:])
-        res = np.roll(data, factor)
-        print(res[:10], res[-10:])
         return np.roll(data, factor)
         
 
@@ -123,7 +118,7 @@ class MODWT:
         print("taille W", len(W))
 
         for j, W_j in enumerate(W):            
-            ax = axes[j]
+            ax = axes[-j -2 ]
             factor = self.shift_factor_H(j + 1, 8) # make the lenght of the filter to compute automatically
             print(factor)
             W_j_shifted = self.circular_shift(factor, W_j)
@@ -131,7 +126,7 @@ class MODWT:
             ax.set_ylabel("W%d" % (j+1))
             ax.set_xlim(0, len(self.data) - 1)
         
-        ax = axes[len(W)]
+        ax = axes[0     ]
         factor = self.shift_factor_G(len(W), 8)
         V_shifted = self.circular_shift(factor, V)
         ax.plot(V_shifted, 'm')
@@ -141,12 +136,122 @@ class MODWT:
         ax = axes[len(W) + 1]
         ax.set_ylabel('ECG Signal')
         ax.plot(self.data, 'r')
+        ax.set_xlim(0, len(self.data) - 1)
+        plt.show()
 
+    
+    def plot_mra(self, S, D):
+        fig, axes = plt.subplots(len(D)+2)
+        axes[0].set_title("Multiresolution Analysis")
+        
+        axes[0].set_xlim(0, len(self.data) - 1)
+        for j, D_j in enumerate(D):         
+            
+            ax = axes[-j -2]
+            ax.plot(D_j, 'g')
+            ax.set_ylabel("D%d" % (j+1))
+            ax.set_xlim(0, len(self.data) - 1)
+        
+        ax = axes[0]
+        ax.plot(S, 'm')
+        ax.set_ylabel("S%d" % (len(D)))
+        ax.set_xlim(0, len(self.data) - 1)
+
+        ax = axes[len(D) + 1]
+        ax.set_ylabel('ECG Signal')
+        ax.plot(self.data, 'r')
+        ax.set_xlim(0, len(self.data) - 1)
         plt.show()
 
 
-    def mra(self):
-        pass
+    
+    def imodwt(self, V, W, j, filter):
+        """
+        For one iteration takes V_J0 and compute V_J0 - 1
+        """
+        V_j_minus_1 = []
+        h = self.wavelets[filter]['h']
+        g = self.wavelets[filter]['g']
+        N = len(V)
+        L = len(h)
+        V_j_minus_1
+        for t in range(N):
+            k = t
+            V_j_minus_1_k = h[0] * W[k] + g[0] * V[k]
+            for n in range(1, L) :
+                k = k + 2 ** (j-1)
+                if k >= N : # circular shift
+                    k = k - N 
+                V_j_minus_1_k += h[n] * W[k] + g[n] * V[k]
+            V_j_minus_1.append(V_j_minus_1_k)
+        return V_j_minus_1
+
+    def from_V_to_X(self, V_J, W):
+        J0 = len(W)
+        all_V_j_minus_1 = [
+            V_J
+        ]
+        for scale in range(len(W) -1 , -1, -1):
+            print(scale + 1)
+            all_V_j_minus_1.append(
+                self.imodwt(
+                    all_V_j_minus_1[-1],
+                    W[scale],
+                    scale + 1
+                )
+            )
+        return all_V_j_minus_1[-1]
+
+    
+    def mra(self, V, W, J, filter):
+        
+        D = [] 
+        S = []
+
+        # compute Dj
+        O_j = [0 for i in range(len(V))]
+        # for scale in range(J- 1, -1, -1) : 
+        for scale in range(J): 
+            W_scale = W[scale]
+            
+            D_scale = []
+            D_scale.append(
+                    self.imodwt(
+                        O_j , 
+                        W_scale , 
+                        J, 
+                        filter
+                    )
+                )
+
+            for j in range(scale): 
+                D_scale.append(
+                    self.imodwt(
+                        D_scale[-1] , 
+                        O_j , 
+                        j + 1, 
+                        filter
+                    )
+                )
+            D.append(
+                D_scale[-1] #the last result is D_j
+            )
+        
+        # Let's compute S_j. 
+        S_scale = [V]
+        for scale in range (J - 1, -1, -1):
+            S_scale.append(
+                self.imodwt(
+                    S_scale[-1],
+                    O_j, 
+                    scale + 1, 
+                    filter
+                )
+            )
+
+        S = S_scale[-1]
+        return S, D
+
 
 if __name__ == '__main__':
 
@@ -156,71 +261,19 @@ if __name__ == '__main__':
 
     data = file.channels[5].data
 
-    ecg = data[:1000]
+    
+    ecg = data[:3000]
     print(len(ecg))
-    m = MODWT(ecg, 6)
+    m = WaveletsTransforms(ecg, 4)
+    m.get_wavelet_filters("/home/julie/Documents/IRBA/dev/initial_approaches/wavelets/wavelets.json")
 
+    V, W = m.modwt('db6')
 
-    V, W = m.modwt()
+    
     m.plot_modwt(V, W)
 
- 
+    S, D = m.mra(V, W, 4, 'db6')
 
-    # dwt = MODWT(ecg)
-    # dwt.max_j()
-    # dwt.modwt()
-    # ecg = pywt.data.ecg()
+    m.plot_mra(S, D)
+    
 
-    # # set trim_approx to avoid keeping approximation coefficients for all levels
-
-    # # set norm=True to rescale the wavelets so that the transform partitions the
-    # # variance of the input signal among the various coefficient arrays.
-
-    # coeffs = pywt.swt(ecg, wavelet='db10', trim_approx=True, norm=True, level = 6)
-
-    # # coeffs = pywt.mra(ecg, wavelet='db4', transform='swt')
-
-    # ca = coeffs[0]
-    # details = coeffs[1:]
-
-    # print("Variance of the ecg signal = {}".format(np.var(ecg, ddof=1)))
-
-    # variances = [np.var(c, ddof=1) for c in coeffs]
-    # detail_variances = variances[1:]
-    # print("Sum of variance across all SWT coefficients = {}".format(
-    #     np.sum(variances)))
-
-    # # Create a plot using the same y axis limits for all coefficient arrays to
-    # # illustrate the preservation of amplitude scale across levels when norm=True.
-    # ylim = [ecg.min(), ecg.max()]
-
-    # fig, axes = plt.subplots(len(coeffs) + 1)
-    # axes[0].set_title("normalized SWT decomposition")
-    # axes[0].plot(ecg)
-    # axes[0].set_ylabel('ECG Signal')
-    # axes[0].set_xlim(0, len(ecg) - 1)
-    # axes[0].set_ylim(ylim[0], ylim[1])
-
-    # for i, x in enumerate(coeffs):
-    #     ax = axes[-i - 1]
-    #     ax.plot(coeffs[i], 'g')
-    #     if i == 0:
-    #         ax.set_ylabel("A%d" % (len(coeffs) - 1))
-    #     else:
-    #         ax.set_ylabel("D%d" % (len(coeffs) - i))
-    #     # Scale axes
-    #     ax.set_xlim(0, len(ecg) - 1)
-    #     ax.set_ylim(ylim[0], ylim[1])
-
-
-    # # reorder from first to last level of coefficients
-    # level = np.arange(1, len(detail_variances) + 1)
-
-    # # create a plot of the variance as a function of level
-    # # plt.figure(figsize=(8, 6))
-    # # fontdict = dict(fontsize=16, fontweight='bold')
-    # # plt.plot(level, detail_variances[::-1], 'k.')
-    # # plt.xlabel("Decomposition level", fontdict=fontdict)
-    # # plt.ylabel("Variance", fontdict=fontdict)
-    # # plt.title("Variances of detail coefficients", fontdict=fontdict)
-    plt.show()
